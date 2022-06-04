d3f044becc4ca75959020e906091e3fd8903fba2
Sam Smith <korhan.tokgoz92@gmail.com>
2022-06-04 23:36:53 +0300

    Updates

---
+++ precache-manifest.16e45dfc4a570cf1f531a6397a48a4c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe153f98d129531d06314e5888bb55b4",
+    "url": "/Tokgoz-Restaurant-Menu-Project/index.html"
+  },
+  {
+    "revision": "b14bb0acac7987edf2df",
+    "url": "/Tokgoz-Restaurant-Menu-Project/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Tokgoz-Restaurant-Menu-Project/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tokgoz-Restaurant-Menu-Project/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14bb0acac7987edf2df",
+    "url": "/Tokgoz-Restaurant-Menu-Project/static/js/main.29ef34fd.chunk.js"
+  },
+  {
+    "revision": "5463b929e2fe6cdc07f0",
+    "url": "/Tokgoz-Restaurant-Menu-Project/static/js/runtime-main.58c294af.js"
+  }
+]);
